1d602b81fbd1063272e94887eaf659fed727f802
hhidayatinc <chasanah55@gmail.com>
2021-03-21 14:57:56 +0700

    player movement

---
+++ SpaceSave/Assets/Scripts/AstronotMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstronotMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+	public Animator animator;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
+	}
+
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+		jump = false;
+	}
+}
---
+++ SpaceSave/Assets/Scripts/Crystals.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystals : MonoBehaviour
+{
+    public Text crystalText;
+    private int totalCrystals;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateCrystalText();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D hit) {
+        if(hit.CompareTag("Crystal")){
+            totalCrystals++;
+            UpdateCrystalText();
+            Destroy(hit.gameObject);
+        }
+    }
+
+    private void UpdateCrystalText(){
+        string crystalMessage = "Crystals = " + totalCrystals;
+        crystalText.text = crystalMessage;
+    }
+}
