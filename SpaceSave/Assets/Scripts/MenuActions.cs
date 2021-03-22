8931f505b6b14aaf53434761d8b335fe112f8337
Annastasya Putri Ivory <64578529+annastasyaivory@users.noreply.github.com>
2021-03-22 11:26:06 +0700

    bikin menu

---
+++ SpaceSave/Assets/Scripts/MenuActions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour
+{
+    public void MENU_ACTION_GoToPage(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
