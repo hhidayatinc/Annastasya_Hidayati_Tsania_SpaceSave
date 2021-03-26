b076932940d33dcfe5585f6f4eac1b22e709a894
hhidayatinc <chasanah55@gmail.com>
2021-03-26 19:23:25 +0700

    nambah asteroid dan hati

--- SpaceSave/Assets/Scripts/Crystal.cs
+++ SpaceSave/Assets/Scripts/Crystal.cs
@@ -4,12 +4,15 @@ using UnityEngine.UI;
 
 public class Crystal : MonoBehaviour
 {
+    private PlayerInventoryDisplay playerInventoryDisplay;
     public Text crystalText;
     private int totalCrystal=0;
+    private int totalAsteroid=0;
     // Start is called before the first frame update
     void Start()
     {
         UpdateCrystalText();
+        playerInventoryDisplay = GetComponent<PlayerInventoryDisplay>();
     }
 
     // Update is called once per frame
@@ -25,6 +28,11 @@ public class Crystal : MonoBehaviour
             UpdateCrystalText();
             Destroy(hit.gameObject);
         }
+        if(hit.CompareTag("Asteroid")){
+                totalAsteroid++;
+                playerInventoryDisplay.OnChangeHeartTotal(totalAsteroid);
+                Destroy(hit.gameObject);
+            }
         
     }
 }
---
+++ SpaceSave/Assets/Scripts/PlayerInventoryDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventoryDisplay : MonoBehaviour
+{
+   public Image[] heartPlaceholders;
+ 	public Sprite iconFullHeart;
+ 	public Sprite iconEmptyHeart;
+ 	public void OnChangeHeartTotal(int asteroidTotal)
+ 	{
+ 		for (int i = 0; i < heartPlaceholders.Length; ++i)
+ 			{
+ 				if (i < asteroidTotal)
+ 					heartPlaceholders[i].sprite = iconEmptyHeart;
+ 				else
+ 					heartPlaceholders[i].sprite = iconFullHeart;
+ 			}
+ 	}
+}
