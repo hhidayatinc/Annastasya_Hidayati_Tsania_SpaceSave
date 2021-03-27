2f00246bd3e2d5c9c828c594b4a7545e7a199958
Annastasya Putri Ivory <64578529+annastasyaivory@users.noreply.github.com>
2021-03-28 02:03:51 +0700

    menu level complete

---
+++ SpaceSave/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public GameObject Endscrn;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Endscrn.gameObject.SetActive(true);
+        }
+    }
+
+    private void Awake()
+    {
+        Endscrn.gameObject.SetActive(false);
+    }
+}
